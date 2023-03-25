c1a89726ec87deac31360856fd1e4415beb731ac
Erick Chimal <125524019+Erick-Chimal@users.noreply.github.com>
2023-03-25 13:35:36 -0700

    Create nmap_automation.py

---
+++ nmap_automation.py
@@ -0,0 +1,15 @@
+import OS
+welcome = "Nmap scanner"
+x = welcome.center(155, "-")
+print(x)
+IP = input("Target IP: ")
+target = (f"Scanning {IP}")
+target_ip = target.center(155, "-")
+print(target_ip)
+scan_type = int(input("Select your mode of scannning\n1) Quick Scan\n2) Passive Scan\n3) Aggressive scan\n"))
+if scan_type == 1:
+  os.system(f"sudo nmap -sV -sC -A -T4 {IP} -oN nmap_results.txt")
+elif scan_type == 2:
+  os.system(f"sudo nmap -sV -sV -A -T1 {IP} -oN nmap_results.txt")
+else:
+  os.system(f"sudo nmap -sV -sC -A -T4 -p- {IP} -oN nmap_results.txt")
